a1c380696dcd3cfb64b2f2cfa5049e175bbf75a7
Layomi22 <sewejlayomi18@gmail.com>
2022-11-30 02:36:30 -0800

    if then

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Grace Adeniyi
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Grace Adeniyi
+
+def pow(a, b):
+    return (a ** b)
